b937a8ead9da28bef6721ebea8521786779c92b8
Mathieu Lemay <acidrain1@gmail.com>
2024-03-24 16:52:55 -0400

    WIP

---
+++ ff6_save_editor/encryption.py
@@ -0,0 +1,44 @@
+from typing import cast
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from py3rijndael import RijndaelCbc, ZeroPadding  # ty_pe: ignore[import-untyped]
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EncryptionSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="FF6_EDITOR_", env_file=".env")
+
+    password: bytes
+    salt: bytes
+    block_size: int = 32
+
+
+class EncryptionManager:
+    def __init__(self, encryption_settings: EncryptionSettings | None = None) -> None:
+        encryption_settings = encryption_settings or EncryptionSettings()
+
+        algo = hashes.SHA1()  # noqa: S303: Insecure algorithm. Not my choice.
+        kdf = PBKDF2HMAC(algo, length=64, salt=encryption_settings.salt, iterations=10)
+        key_iv_bytes = kdf.derive(encryption_settings.password)
+
+        key = key_iv_bytes[: encryption_settings.block_size]
+        iv = key_iv_bytes[encryption_settings.block_size :]
+
+        self.cipher = RijndaelCbc(
+            key=key,
+            iv=iv,
+            padding=ZeroPadding(encryption_settings.block_size),
+            block_size=encryption_settings.block_size,
+        )
+
+    def encrypt(self, data: bytes) -> bytes:
+        return cast(bytes, self.cipher.encrypt(data))
+
+    def decrypt(self, data: bytes) -> bytes:
+        plaintext = cast(bytes, self.cipher.decrypt(data))
+        padding = b"\x00" * (len(data) - len(plaintext))
+
+        return plaintext + padding
+
+
--- ff6_save_editor/main.py
+++ ff6_save_editor/main.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import json
 import sys
 import zlib
@@ -5,50 +7,12 @@ from datetime import datetime
 from pathlib import Path
 from typing import Any, Generic, TypeVar, cast
 
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from py3rijndael import RijndaelCbc, ZeroPadding  # type: ignore[import-untyped]
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import Field, field_serializer, field_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from ff6_save_editor.encryption import EncryptionManager
 
 JsonDict = dict[str, Any]
-
-
-class EncryptionSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="FF6_EDITOR_")
-
-    password: bytes
-    salt: bytes
-    block_size: int = 32
-
-
-class Encryption:
-    def __init__(self, encryption_settings: EncryptionSettings | None = None) -> None:
-        encryption_settings = encryption_settings or EncryptionSettings()
-
-        algo = hashes.SHA1()  # noqa: S303: Insecure algorithm. Not my choice.
-        kdf = PBKDF2HMAC(algo, length=64, salt=encryption_settings.salt, iterations=10)
-        key_iv_bytes = kdf.derive(encryption_settings.password)
-
-        key = key_iv_bytes[: encryption_settings.block_size]
-        iv = key_iv_bytes[encryption_settings.block_size :]
-
-        self.cipher = RijndaelCbc(
-            key=key,
-            iv=iv,
-            padding=ZeroPadding(encryption_settings.block_size),
-            block_size=encryption_settings.block_size,
-        )
-
-    def encrypt(self, data: bytes) -> bytes:
-        return cast(bytes, self.cipher.encrypt(data))
-
-    def decrypt(self, data: bytes) -> bytes:
-        plaintext = cast(bytes, self.cipher.decrypt(data))
-        padding = b"\x00" * (len(data) - len(plaintext))
-
-        return plaintext + padding
 
 
 class BaseModel(PydanticBaseModel):
@@ -175,7 +139,7 @@ class SaveModel(BaseModel):
 def load(src: Path) -> SaveModel:
     buffer = src.read_bytes()
 
-    enc = Encryption()
+    enc = EncryptionManager()
     decrypted = enc.decrypt(buffer)
 
     raw = zlib.decompress(decrypted, wbits=-zlib.MAX_WBITS)
@@ -189,7 +153,7 @@ def save(save: SaveModel, dst: Path) -> None:
     raw = save.model_dump_json()
     buffer = zlib.compress(raw.encode(), wbits=-zlib.MAX_WBITS)
 
-    enc = Encryption()
+    enc = EncryptionManager()
     buffer = enc.encrypt(buffer)
 
     dst.write_bytes(buffer)
